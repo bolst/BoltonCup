cb8b1a67cb6fb6282ea65b69e41ae39a81975e8f
nic <bolton21@uwindsor.ca>
2024-07-10 09:42:30 -0400

    upd

--- BoltonCup/BoltonCup/obj/Release/net8.0/BoltonCup.AssemblyInfo.cs
+++ BoltonCup/BoltonCup/obj/Release/net8.0/BoltonCup.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
     "is a component library built on top of Blazor and CSS frameworks like\r\n\t\t\tBootst" +
     "rap, FluentUI2, Tailwind, Bulma and Material.\r\n\t\t")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.5.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.5+f8a861c4460998f56292a5b970cbe0bc9136bab1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.5+b5442ed9a31fcc1987591908946d5147411b3880")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoltonCup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoltonCup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.5.0.0")]
