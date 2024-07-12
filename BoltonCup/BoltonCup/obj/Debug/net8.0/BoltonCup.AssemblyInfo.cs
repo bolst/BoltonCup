7f980bd1ae4c590f746516bf0539f8ba7afab222
Chris Bolton <chris8bolton@icloud.com>
2024-07-11 22:20:16 -0400

    init

--- BoltonCup/BoltonCup/obj/Debug/net8.0/BoltonCup.AssemblyInfo.cs
+++ BoltonCup/BoltonCup/obj/Debug/net8.0/BoltonCup.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
     " a component library built on top of Blazor and CSS frameworks like\n\t\t\tBootstrap" +
     ", FluentUI2, Tailwind, Bulma and Material.\n\t\t")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.5.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.5+e5a539f13fb11c86cf53bcb655a6abca5e60ef7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.5+41c4a4c3f94e0799a523208711f69ef2ea5163cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoltonCup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoltonCup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.5.0.0")]
