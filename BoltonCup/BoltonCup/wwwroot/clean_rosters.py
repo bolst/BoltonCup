36e8db4f3f8562b29d4c23f46ba020f72fa95e2e
nic <bolton21@uwindsor.ca>
2024-07-18 19:10:28 -0400

    add script to clean rosters

---
+++ BoltonCup/BoltonCup/wwwroot/clean_rosters.py
@@ -0,0 +1,21 @@
+import json
+
+def team_file(n:int) -> str: return f'team{n}.json'
+
+def read_file(n:int) -> dict:
+    with open(team_file(n), 'r') as rf:
+        return json.load(rf)
+
+def write_file(n:int, data):
+    with open(team_file(n), 'w') as wf:
+        json.dump(data, wf)
+
+def clean_files():
+    for n in [1,2,3,4]:
+        data = read_file(n)
+        data['players'] = []
+        write_file(n, data)
+
+
+if __name__ == '__main__':
+    clean_files()
