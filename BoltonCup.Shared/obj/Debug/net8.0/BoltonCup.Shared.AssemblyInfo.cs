84b29507847749ed1b3b524724954f82153b6315
nic <bolton21@uwindsor.ca>
2025-03-03 11:29:52 -0500

    - add carousel component and separate web and app home page

--- BoltonCup.Shared/obj/Debug/net8.0/BoltonCup.Shared.AssemblyInfo.cs
+++ BoltonCup.Shared/obj/Debug/net8.0/BoltonCup.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoltonCup.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f93fe6b6896e2891f04716f10f0e496664bf407")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de531750be7c6e6c4fd829f65394f01b66b5740b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoltonCup.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoltonCup.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
