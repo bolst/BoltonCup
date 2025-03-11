78db4c2d8d5ff86f61207c24c69c4bcac7eae006
nic <bolton21@uwindsor.ca>
2025-03-10 20:22:22 -0400

    - disclaimer for registration

--- BoltonCup.Shared/obj/Debug/net8.0/BoltonCup.Shared.AssemblyInfo.cs
+++ BoltonCup.Shared/obj/Debug/net8.0/BoltonCup.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoltonCup.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bec050433106fdb8d2b4328c8cf0f273ebad36c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87c8c8e60ec7b44b2c31bbd369f5bc8292f5d04d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoltonCup.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoltonCup.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
