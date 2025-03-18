38cf9096bdecfbcfd91d3d0e8e87ff6d1f6233d4
nic <bolton21@uwindsor.ca>
2025-03-17 23:36:29 -0400

    - update nuget

--- BoltonCup.Shared/obj/Debug/net8.0/BoltonCup.Shared.AssemblyInfo.cs
+++ BoltonCup.Shared/obj/Debug/net8.0/BoltonCup.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoltonCup.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11eaa3cab509ea7a1b821778ad5504533f7ea575")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abadf4a647da8a277e762835b790941f39a2b3ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoltonCup.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoltonCup.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
