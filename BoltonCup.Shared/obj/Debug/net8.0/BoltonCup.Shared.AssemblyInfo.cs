4e312c148e74de2dca3e0ba06fb2050b909c3c8c
nic <bolton21@uwindsor.ca>
2025-03-19 17:16:57 -0400

    - begin refactoring some of models/queries

--- BoltonCup.Shared/obj/Debug/net8.0/BoltonCup.Shared.AssemblyInfo.cs
+++ BoltonCup.Shared/obj/Debug/net8.0/BoltonCup.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoltonCup.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a1ed92ae63c05d6f80ce1bba61f43aca9013f3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89bdfe179b02bc16f6bcab7f2cc789440a02a5c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoltonCup.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoltonCup.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
