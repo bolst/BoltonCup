19fd0f7b55e389cc4d9aee733bb3114858438114
nic <bolton21@uwindsor.ca>
2025-04-17 12:37:47 -0400

    - remove i agree on register form

--- BoltonCup.Shared/obj/Debug/net8.0/BoltonCup.Shared.AssemblyInfo.cs
+++ BoltonCup.Shared/obj/Debug/net8.0/BoltonCup.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoltonCup.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f670f7b64a5d9cf70a4a69726f4ff8b8a33138")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6b1394ad7d62ed979ea5afe05f1eb165173a321")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoltonCup.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoltonCup.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
