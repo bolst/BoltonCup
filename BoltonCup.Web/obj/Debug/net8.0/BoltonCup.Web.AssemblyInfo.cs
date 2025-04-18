7b0f2749cbc5e12f2c695f2bcbd3c068c52a00df
nic <bolton21@uwindsor.ca>
2025-04-17 20:26:54 -0400

    - add tournament selection to stat page

--- BoltonCup.Web/obj/Debug/net8.0/BoltonCup.Web.AssemblyInfo.cs
+++ BoltonCup.Web/obj/Debug/net8.0/BoltonCup.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoltonCup.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57b7f1bfc9d290fe6551bfe7d43290485768bcd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0659a2de55cd2cd94724bfaf47c7f4f72bec699d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoltonCup.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoltonCup.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
