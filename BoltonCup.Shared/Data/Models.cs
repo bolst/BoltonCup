c50bc7f06f80927c1ec8fd241d6baa57e36e8a8f
nic <bolton21@uwindsor.ca>
2025-04-18 22:04:56 -0400

    - simplify penalty query

--- BoltonCup.Shared/Data/Models.cs
+++ BoltonCup.Shared/Data/Models.cs
@@ -112,9 +112,10 @@ public class GamePenalty
     public TimeSpan Time { get; set; }
     public int Period { get; set; }
     public int TeamId { get; set; }
-    public string Infraction { get; set; } = "";
+    public string Infraction { get; set; }
     public int PlayerId { get; set; }
-    public string PlayerName { get; set; } = "";
+    public string PlayerName { get; set; }
+    public string TeamLogo { get; set; }
 }
 
 public class PlayerStatLine
