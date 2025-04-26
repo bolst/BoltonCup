762fbc82294f90add2b088a7f2b54a0d2ec5cdd5
nic <bolton21@uwindsor.ca>
2025-04-25 20:43:36 -0400

    - draft logic

--- BoltonCup.Draft/Data/DraftServiceProvider.cs
+++ BoltonCup.Draft/Data/DraftServiceProvider.cs
@@ -14,40 +14,56 @@ public class DraftServiceProvider
     
     public int DraftId => DRAFT_ID;
 
-    public async Task<BCTeam> GetTeamWithCurrentPick()
+    public async Task<(BCTeam, BCDraftPick)> GetTeamWithCurrentPick()
     {
         var currentPick = await _bcData.GetMostRecentDraftPickAsync(DRAFT_ID);
 
         if (currentPick is null)
         {
             int order = 1;
+
+            var team = (await _bcData.GetTeamByDraftOrderAsync(DRAFT_ID, order))!;
             
-            return (await _bcData.GetTeamByDraftOrderAsync(DRAFT_ID, order))!;
+            var pick = new BCDraftPick
+            {
+                draft_id = DRAFT_ID,
+                pick = 1,
+                round = 1,
+            };
+            
+            return (team, pick);
         }
         else
         {
-            int round = currentPick.round;
-            int pick = currentPick.pick;
+            int order = currentPick.pick + 1;
 
-            int order = pick;
-
-            if (round % 2 == 0)
+            if (currentPick.round % 2 == 0)
             {
-                order = 6 - pick + 1;
+                order = 6 - currentPick.pick + 1;
             }
             
+            var team = (await _bcData.GetTeamByDraftOrderAsync(DRAFT_ID, order))!;
             
-            return (await _bcData.GetTeamByDraftOrderAsync(DRAFT_ID, order))!;
+            var pick = new BCDraftPick
+            {
+                draft_id = DRAFT_ID,
+                pick = currentPick.pick == 6 ? 1 : currentPick.pick + 1,
+                round = currentPick.pick == 6 ? (currentPick.round + 1) : currentPick.round,
+            };
+            
+            return (team, pick);
         }
     }
 
     public async Task DraftPlayerAsync(PlayerProfile player)
     {
-        var team = await GetTeamWithCurrentPick();
+        var (team, pick) = await GetTeamWithCurrentPick();
         Console.WriteLine($"{team.name} is drafting {player.name}");
         
         // TODO:
         // 1. update database to indicate the drafted player is now on the team with current pick
+        await _bcData.DraftPlayerAsync(player, team, pick);
+        
         // 2. notify subscribers that a selection has been made (e.g., timer)
     }
 
@@ -59,8 +75,8 @@ public class DraftServiceProvider
         return teams.OrderBy(t => draftOrder.First(d => d.team_id == t.id).order);
     }
 
-    public async Task<IEnumerable<PlayerProfile>> GetTeamRosterAsync(BCTeam team)
+    public async Task<IEnumerable<object>> GetSelectedPlayersAsync()
     {
-        return await _bcData.GetRosterByTeamId(team.id);
+        throw new NotImplementedException();
     }
 }
