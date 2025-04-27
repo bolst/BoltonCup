01bc310ee0c6e7363aeed194a8be907ab8990efc
nic <bolton21@uwindsor.ca>
2025-04-27 01:56:22 -0400

    - update to use GetAsync instead of Get in Stripe

--- BoltonCup.Shared/Data/Services.cs
+++ BoltonCup.Shared/Data/Services.cs
@@ -87,7 +87,7 @@ public class StripeServiceProvider
         try
         {
             var service = new Stripe.Checkout.SessionService();
-            Stripe.Checkout.Session session = service.Get(checkoutId);
+            Stripe.Checkout.Session session = await service.GetAsync(checkoutId);
 
             var email = session.CustomerDetails.Email;
             if (string.IsNullOrEmpty(email))
