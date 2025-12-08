9c92cb2284564741db0e5ed25ab1bf471a0fdfde
nic <bolton21@uwindsor.ca>
2025-12-08 10:14:53 -0500

    IRepository: rename TIndex to TKey

--- src/BoltonCup.WebAPI/Interfaces/IRepository.cs
+++ src/BoltonCup.WebAPI/Interfaces/IRepository.cs
@@ -2,11 +2,11 @@
 
 namespace BoltonCup.WebAPI.Interfaces;
 
-public interface IRepository<TModel, in TIndex> where TModel : class
+public interface IRepository<TModel, in TKey> where TModel : class
 {
     Task<IEnumerable<TModel>> GetAllAsync();
-    Task<TModel?> GetByIdAsync(TIndex id);
+    Task<TModel?> GetByIdAsync(TKey id);
     Task<bool> AddAsync(TModel entity);
     Task<bool> UpdateAsync(TModel entity);
-    Task<bool> DeleteAsync(TIndex id);
+    Task<bool> DeleteAsync(TKey id);
 }
