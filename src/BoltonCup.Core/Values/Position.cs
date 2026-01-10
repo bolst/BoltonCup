11765b0c445786af7b97fc20f575bbc543c5a46b
nic <bolton21@uwindsor.ca>
2026-01-10 12:18:52 -0500

    Core: create Values with Position class

---
+++ src/BoltonCup.Core/Values/Position.cs
@@ -0,0 +1,8 @@
+namespace BoltonCup.Core.Values;
+
+public static class Position
+{
+    public const string Forward = "forward";
+    public const string Defense = "defense";
+    public const string Goalie = "goalie";
+}
