712e34e9f2b6cf75b9d8cd95471f9f34c816a0cb
nic <bolton21@uwindsor.ca>
2026-01-11 13:42:31 -0500

    create IReadOnlyRepository

---
+++ src/BoltonCup.Core/Interfaces/Base/IReadOnlyRepository.cs
@@ -0,0 +1,14 @@
+using BoltonCup.Core.Mappings;
+
+namespace BoltonCup.Core.Base;
+
+public interface IReadOnlyRepository<TModel, in TGetQuery, in TKey> where TModel : class
+{
+    Task<IEnumerable<TModel>> GetAllAsync(TGetQuery query);
+    Task<IEnumerable<TResult>> GetAllAsync<TResult>(TGetQuery query)
+        where TResult : IMappable<TModel, TResult>;
+    
+    Task<TModel?> GetByIdAsync(TKey id);
+    Task<TResult?> GetByIdAsync<TResult>(TKey id)
+        where TResult : IMappable<TModel, TResult>;
+}
--- src/BoltonCup.Core/Interfaces/Base/IRepository.cs
+++ src/BoltonCup.Core/Interfaces/Base/IRepository.cs
@@ -1,19 +1,8 @@
-
-
-using BoltonCup.Core.Mappings;
-
 namespace BoltonCup.Core.Base;
 
-public interface IRepository<TModel, in TGetQuery, in TKey> where TModel : class
+public interface IRepository<TModel, in TGetQuery, in TKey> 
+    : IReadOnlyRepository<TModel, TGetQuery, TKey> where TModel : class
 {
-    Task<IEnumerable<TModel>> GetAllAsync(TGetQuery query);
-    Task<IEnumerable<TResult>> GetAllAsync<TResult>(TGetQuery query)
-        where TResult : IMappable<TModel, TResult>;
-    
-    Task<TModel?> GetByIdAsync(TKey id);
-    Task<TResult?> GetByIdAsync<TResult>(TKey id)
-        where TResult : IMappable<TModel, TResult>;
-    
     Task<bool> AddAsync(TModel entity);
     Task<bool> UpdateAsync(TModel entity);
     Task<bool> DeleteAsync(TKey id);
