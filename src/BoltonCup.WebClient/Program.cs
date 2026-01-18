38bfad824bcf4197c8d5c7438084ceb46134f35b
nic <bolton21@uwindsor.ca>
2026-01-18 17:00:58 -0500

    interfaces and date ranges

--- src/BoltonCup.WebClient/Program.cs
+++ src/BoltonCup.WebClient/Program.cs
@@ -10,7 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddScoped(sp =>
+builder.Services.AddScoped<IBoltonCupApi>(sp =>
 {
     var baseUrl = builder.Configuration["BoltonCupApi:BaseUrl"] 
                   ?? throw new InvalidOperationException("BoltonCupApi:BaseUrl configuration is missing.");
