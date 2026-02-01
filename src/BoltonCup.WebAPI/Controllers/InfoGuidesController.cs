aa7b21150b203eec7590d7c6aab8297f6e0e9428
nic <bolton21@uwindsor.ca>
2026-02-01 12:51:33 -0500

    add InfoGuidesController

---
+++ src/BoltonCup.WebAPI/Controllers/InfoGuidesController.cs
@@ -0,0 +1,24 @@
+using BoltonCup.WebAPI.Dtos;
+using BoltonCup.Core;
+using BoltonCup.Infrastructure.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoltonCup.WebAPI.Controllers;
+
+public class InfoGuidesController(IInfoGuideRepository _infoGuides) : BoltonCupControllerBase
+{
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<ActionResult<PaginatedList<InfoGuideDetailDto>>> GetInfoGuides([FromQuery] GetInfoGuidesQuery query)
+    {
+        return Ok(await _infoGuides.GetAllAsync<InfoGuideDetailDto>(query));
+    }
+
+    [AllowAnonymous]
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<InfoGuideSingleDetailDto>> GetInfoGuideById(Guid id)
+    {
+        return OkOrNotFound(await _infoGuides.GetByIdAsync<InfoGuideSingleDetailDto>(id));
+    }
+}
