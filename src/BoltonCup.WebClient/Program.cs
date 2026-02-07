b9c6c0a81c3dea46db74033379dd3ca7c38f8542
nic <bolton21@uwindsor.ca>
2026-02-07 16:34:23 -0500

    add UserMenu to WebClient with auth

--- src/BoltonCup.WebClient/Program.cs
+++ src/BoltonCup.WebClient/Program.cs
@@ -1,23 +1,15 @@
+using BoltonCup.Common;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BoltonCup.WebClient.Components;
 using MudBlazor.Services;
-using BoltonCup.Sdk;
 using MudBlazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddScoped<IBoltonCupApi>(sp =>
-{
-    var baseUrl = builder.Configuration["BoltonCupApi:BaseUrl"] 
-                  ?? throw new InvalidOperationException("BoltonCupApi:BaseUrl configuration is missing.");
-    var httpClient = sp.GetRequiredService<HttpClient>();
-    return new BoltonCupApi(baseUrl, httpClient);
-});
+builder.Services.AddBoltonCupCommonServices(builder.Configuration);
 
 builder.Services.AddMudServices();
 builder.Services.AddMudMarkdownServices();
