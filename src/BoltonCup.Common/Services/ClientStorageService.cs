4bd95772f430869eb2e5c60efcfdaf39fde39949
nic <bolton21@uwindsor.ca>
2026-03-11 17:26:28 -0400

    fix ClientStorageService

--- src/BoltonCup.Common/Services/ClientStorageService.cs
+++ src/BoltonCup.Common/Services/ClientStorageService.cs
@@ -6,16 +6,15 @@ namespace BoltonCup.Common.Services;
 public class ClientStorageService(IBoltonCupApi _boltonCupApi) 
     : IStorageService
 {
-    public async Task<UploadCredentials> GenerateUploadCredentialsAsync(string fileExtension, string contentType, CancellationToken cancellationToken = default)
+    public async Task<Core.UploadCredentials> GenerateUploadCredentialsAsync(string fileExtension, string contentType, CancellationToken cancellationToken = default)
     { 
-        var result = await _boltonCupApi.GeneratePreSignedPutUrlAsync(fileExtension, contentType, cancellationToken);
-        return new UploadCredentials(result.UploadUrl, result.TempKey);
+        var result = await _boltonCupApi.GenerateUploadCredentialsAsync(fileExtension, contentType, cancellationToken);
+        return new Core.UploadCredentials(result.UploadUrl, result.TempKey);
     }
 
     public Task CopyAssetAsync(string sourceKey, string destinationKey,
         CancellationToken cancellationToken = default)
     {
-        // TODO
-        return Task.CompletedTask;
+        throw new NotImplementedException("ClientStorageService does not support copying assets. This method should not be called on the client.");
     }
 }
