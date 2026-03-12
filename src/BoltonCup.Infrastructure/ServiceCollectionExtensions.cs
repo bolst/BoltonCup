76d7351448ae9934a6e5619e8f29fac84f7c969b
nic <bolton21@uwindsor.ca>
2026-03-12 10:46:51 -0400

    implement AssetUrlResolver in Infrastructure

--- src/BoltonCup.Infrastructure/ServiceCollectionExtensions.cs
+++ src/BoltonCup.Infrastructure/ServiceCollectionExtensions.cs
@@ -48,6 +48,7 @@ public static class ServiceCollectionExtensions
         var accountId = r2Config["AccountId"];
         var accessKey = r2Config["AccessKey"];
         var secretKey = r2Config["SecretKey"];
+        var baseUrl = r2Config["BaseUrl"];
         
         var s3Credentials = new Amazon.Runtime.BasicAWSCredentials(accessKey, secretKey);
         var s3Config = new AmazonS3Config
@@ -58,6 +59,7 @@ public static class ServiceCollectionExtensions
         return builder.Services
             .AddSingleton<IAmazonS3>(_ => new AmazonS3Client(s3Credentials, s3Config))
             .AddSingleton<IAssetKeyGenerator, AssetKeyGenerator>()
+            .AddSingleton<IAssetUrlResolver, AssetUrlResolver>(_ => new AssetUrlResolver(baseUrl!))
             .Replace(ServiceDescriptor.Singleton<IStorageService, ServerStorageService>());
     }
 }
