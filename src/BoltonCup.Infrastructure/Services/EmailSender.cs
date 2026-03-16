4983964aba09de131e1e8c97e763e9780af991ce
nic <bolton21@uwindsor.ca>
2026-03-16 15:31:51 -0400

    EmailSender: logs

--- src/BoltonCup.Infrastructure/Services/EmailSender.cs
+++ src/BoltonCup.Infrastructure/Services/EmailSender.cs
@@ -1,22 +1,26 @@
 using BoltonCup.Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
 
 namespace BoltonCup.Infrastructure.Services;
 
-public class EmailSender : IEmailSender<BoltonCupUser> 
+public class EmailSender(ILogger<EmailSender> _logger) : IEmailSender<BoltonCupUser> 
 {
     public Task SendConfirmationLinkAsync(BoltonCupUser user, string email, string confirmationLink)
     {
+        _logger.LogInformation("Sending confirmation link to {UserName} with email {email}", user.UserName, email);
         throw new NotImplementedException();
     }
 
     public Task SendPasswordResetLinkAsync(BoltonCupUser user, string email, string resetLink)
     {
+        _logger.LogInformation("Sending password reset link to {UserName} with email {email}", user.UserName, email);
         throw new NotImplementedException();
     }
 
     public Task SendPasswordResetCodeAsync(BoltonCupUser user, string email, string resetCode)
     {
+        _logger.LogInformation("Sending password reset code to {UserName} with email {email}", user.UserName, email);
         throw new NotImplementedException();
     }
 }
