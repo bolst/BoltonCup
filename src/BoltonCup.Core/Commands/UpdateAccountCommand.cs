d75c4eb5e598efb1762644d07debae510adeebee
nic <bolton21@uwindsor.ca>
2026-03-21 17:36:20 -0400

    AccountMapper: add ToCommand

--- src/BoltonCup.Core/Commands/UpdateAccountCommand.cs
+++ src/BoltonCup.Core/Commands/UpdateAccountCommand.cs
@@ -1,11 +1,10 @@
 namespace BoltonCup.Core.Commands;
 
-public sealed record UpdateAccountCommand
-{
-    public required int AccountId { get; init; }
-    public required string FirstName { get; set; }
-    public required string LastName { get; set; }
-    public required DateTime Birthday { get; set; }
-    public string? HighestLevel { get; set; }
-    public string? PreferredBeer { get; set; }
-}
+public sealed record UpdateAccountCommand( 
+    int AccountId,
+    string FirstName,
+    string LastName,
+    DateTime Birthday, 
+    string? HighestLevel, 
+    string? PreferredBeer
+);
