57796505b4f6fd766464fa2f98969fb3e62c3481
nic <bolton21@uwindsor.ca>
2026-03-21 17:35:42 -0400

    add account id to claims

--- src/BoltonCup.Common/Auth/CookieAuthenticationStateProvider.cs
+++ src/BoltonCup.Common/Auth/CookieAuthenticationStateProvider.cs
@@ -16,6 +16,7 @@ public class CookieAuthenticationStateProvider(IBoltonCupApi _api) : Authenticat
             {
                 var claims = new List<Claim>
                 {
+                    new(ClaimTypes.Sid, userInfo.Id.ToString()),
                     new(ClaimTypes.Name, userInfo.Name),
                     new(ClaimTypes.Email, userInfo.Email),
                     new(ClaimTypes.Role, string.Join(';', userInfo.Roles))
