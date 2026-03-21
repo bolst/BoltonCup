83a019f2f9c7639ed4f0c97c42192bff6db7a76c
nic <bolton21@uwindsor.ca>
2026-03-21 17:45:28 -0400

    UpdateAccountRequest: add validation attributes

--- src/BoltonCup.WebAPI/Mapping/Requests/UpdateAccountRequest.cs
+++ src/BoltonCup.WebAPI/Mapping/Requests/UpdateAccountRequest.cs
@@ -1,9 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BoltonCup.WebAPI.Mapping;
 
-public record UpdateAccountRequest(
-    string FirstName,
-    string LastName,
-    DateTime Birthday,
-    string? HighestLevel,
-    string? PreferredBeer
-);
+public record UpdateAccountRequest
+{
+    [Required(ErrorMessage = "First name is required.")]
+    [MaxLength(50, ErrorMessage = "First name cannot exceed 50 characters.")]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Last name is required.")]
+    [MaxLength(50)]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Birthday is required.")]
+    public DateTime Birthday { get; set; }
+
+    [MaxLength(50)]
+    public string? HighestLevel { get; set; }
+
+    [MaxLength(100)]
+    public string? PreferredBeer { get; set; }
+}
