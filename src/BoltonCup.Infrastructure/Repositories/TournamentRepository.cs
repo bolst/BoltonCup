f3396d570ce5ec9199f8adf925c13f813c843809
nic <bolton21@uwindsor.ca>
2026-03-22 18:08:55 -0400

    GetTournamentsQuery: add RegistrationOpen

--- src/BoltonCup.Infrastructure/Repositories/TournamentRepository.cs
+++ src/BoltonCup.Infrastructure/Repositories/TournamentRepository.cs
@@ -11,6 +11,7 @@ public class TournamentRepository(BoltonCupDbContext context) : ITournamentRepos
     {
         return await context.Tournaments
             .AsNoTracking()
+            .ConditionalWhere(e => e.IsRegistrationOpen == query.RegistrationOpen!.Value, query.RegistrationOpen.HasValue)
             .Include(e => e.Games)
             .Include(e => e.Teams)
             .ApplySorting(query, x => x.OrderBy(t => t.StartDate))
