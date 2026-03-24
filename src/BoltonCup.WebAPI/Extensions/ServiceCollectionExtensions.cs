51734c9c840cc56f216daebef62adcd259cf2ed7
nic <bolton21@uwindsor.ca>
2026-03-24 09:54:16 -0400

    WebAPI: add handler for access denied redirect

--- src/BoltonCup.WebAPI/Extensions/ServiceCollectionExtensions.cs
+++ src/BoltonCup.WebAPI/Extensions/ServiceCollectionExtensions.cs
@@ -48,6 +48,12 @@ public static class ServiceCollectionExtensions
                     context.Response.StatusCode = StatusCodes.Status401Unauthorized;
                     return Task.CompletedTask;
                 };
+
+                options.Events.OnRedirectToAccessDenied = context =>
+                {
+                    context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                    return Task.CompletedTask;
+                };
             })
             .AddAuthorization(options => 
             {
