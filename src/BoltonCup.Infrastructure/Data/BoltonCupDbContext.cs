de08403fe32ffaeb0e4973c1689b7c856e65c2a2
nic <bolton21@uwindsor.ca>
2026-03-24 16:48:57 -0400

    add default player avatars

--- src/BoltonCup.Infrastructure/Data/BoltonCupDbContext.cs
+++ src/BoltonCup.Infrastructure/Data/BoltonCupDbContext.cs
@@ -44,7 +44,7 @@ public class BoltonCupDbContext(DbContextOptions<BoltonCupDbContext> options)
             entity.Property(e => e.Phone).HasColumnName("phone");
             entity.Property(e => e.Birthday).HasColumnName("birthday");
             entity.Property(e => e.HighestLevel).HasColumnName("highest_level");
-            entity.Property(e => e.Avatar).HasColumnName("avatar_key");
+            entity.Property(e => e.Avatar).HasColumnName("avatar_key").HasDefaultValue(AssetUrlResolver.StaticKeys.PlayerAvatar);
             entity.Property(e => e.Banner).HasColumnName("banner_key").HasDefaultValue(AssetUrlResolver.StaticKeys.PlayerBanner);
             entity.Property(e => e.PreferredBeer).HasColumnName("preferred_beer");
         });
