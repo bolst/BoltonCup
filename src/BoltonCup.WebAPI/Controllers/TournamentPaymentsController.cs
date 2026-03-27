12c6e63bfee812fabb82e33f8cb650f8e2c8177d
nic <bolton21@uwindsor.ca>
2026-03-27 16:41:22 -0400

    separate webhook into its own controller

--- src/BoltonCup.WebAPI/Controllers/TournamentPaymentsController.cs
+++ src/BoltonCup.WebAPI/Controllers/TournamentPaymentsController.cs
@@ -3,7 +3,6 @@ using BoltonCup.Infrastructure.Extensions;
 using BoltonCup.WebAPI.Mapping;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Stripe;
 using static BoltonCup.WebAPI.Authentication.BoltonCupPolicy;
 
 namespace BoltonCup.WebAPI.Controllers;
@@ -12,7 +11,6 @@ namespace BoltonCup.WebAPI.Controllers;
 public class TournamentPaymentsController(ITournamentPaymentService _paymentService, ITournamentPaymentMapper _paymentMapper) 
     : BoltonCupControllerBase
 {
-
     [Authorize(Policy = RequireCompletedAccount)]
     [HttpPost("createIntent")]
     public async Task<ActionResult<TournamentPaymentIntentDto>> CreateTournamentPaymentIntent(int id, [FromBody] CreateTournamentPaymentIntentRequest request)
@@ -21,25 +19,5 @@ public class TournamentPaymentsController(ITournamentPaymentService _paymentServ
         var command = _paymentMapper.ToCommand(id, accountId, request);
         var result = await _paymentService.CreateTournamentPaymentIntentAsync(command);
         return _paymentMapper.ToDto(result);
-    }
-
-    [AllowAnonymous]
-    [HttpPost("stripe/webhook")]
-    public async Task<IActionResult> StripeWebhook()
-    {
-        try
-        {
-            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
-            await _paymentService.ProcessPaymentIntentAsync(json, Request.Headers["Stripe-Signature"].ToString());
-            return Ok();
-        }
-        catch (StripeException e)
-        {
-            return BadRequest();
-        }
-        catch (Exception e)
-        {
-            return StatusCode(500);
-        }
     }
 }
