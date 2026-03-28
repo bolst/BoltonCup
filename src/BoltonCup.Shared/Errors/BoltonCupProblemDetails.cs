ff6d9f69ff043a5561f44888727044452f5e4223
nic <bolton21@uwindsor.ca>
2026-03-28 18:25:57 -0400

    add BoltonCup(Validation)ProblemDetails

---
+++ src/BoltonCup.Shared/Errors/BoltonCupProblemDetails.cs
@@ -0,0 +1,34 @@
+using System.Text.Json.Serialization;
+
+namespace BoltonCup.Shared;
+
+public class BoltonCupProblemDetails
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyOrder(-5)]
+    [JsonPropertyName("type")]
+    public string? Type { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyOrder(-4)]
+    [JsonPropertyName("title")]
+    public string? Title { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyOrder(-3)]
+    [JsonPropertyName("status")]
+    public int? Status { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyOrder(-2)]
+    [JsonPropertyName("detail")]
+    public string? Detail { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyOrder(-1)]
+    [JsonPropertyName("instance")]
+    public string? Instance { get; set; }
+
+    [JsonExtensionData]
+    public IDictionary<string, object?> Extensions { get; set; } = new Dictionary<string, object?>(StringComparer.Ordinal);
+}
---
+++ src/BoltonCup.Shared/Errors/BoltonCupValidationProblemDetails.cs
@@ -0,0 +1,30 @@
+using System.Text.Json.Serialization;
+
+namespace BoltonCup.Shared;
+
+public class BoltonCupValidationProblemDetails : BoltonCupProblemDetails
+{
+    public BoltonCupValidationProblemDetails()
+        : this(new Dictionary<string, string[]>(StringComparer.Ordinal))
+    {
+    }
+
+    public BoltonCupValidationProblemDetails(IDictionary<string, string[]> errors)
+        : this((IEnumerable<KeyValuePair<string, string[]>>)errors)
+    {
+    }
+
+    public BoltonCupValidationProblemDetails(IEnumerable<KeyValuePair<string, string[]>> errors)
+        : this(new Dictionary<string, string[]>(errors ?? throw new ArgumentNullException(nameof(errors)), StringComparer.Ordinal))
+    {
+    }
+
+    private BoltonCupValidationProblemDetails(Dictionary<string, string[]> errors)
+    {
+        Title = "One or more validation errors occurred.";
+        Errors = errors;
+    }
+
+    [JsonPropertyName("errors")]
+    public IDictionary<string, string[]> Errors { get; set; }
+}
