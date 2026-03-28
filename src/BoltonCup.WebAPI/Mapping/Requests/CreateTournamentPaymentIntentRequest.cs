70351bdf247813860e32b2014081b5a5cbff3f99
nic <bolton21@uwindsor.ca>
2026-03-28 12:35:30 -0400

    CreateTournamentPaymentIntentRequest: add error message to attribute

--- src/BoltonCup.WebAPI/Mapping/Requests/CreateTournamentPaymentIntentRequest.cs
+++ src/BoltonCup.WebAPI/Mapping/Requests/CreateTournamentPaymentIntentRequest.cs
@@ -5,6 +5,6 @@ namespace BoltonCup.WebAPI.Mapping;
 
 public record CreateTournamentPaymentIntentRequest
 {
-    [AllowedValues(Forward, Defense, Goalie)]
+    [AllowedValues(Forward, Defense, Goalie, ErrorMessage = $"Position must be one of '{Forward}', '{Defense}', '{Goalie}'")]
     public string Position { get; set; }
 }
