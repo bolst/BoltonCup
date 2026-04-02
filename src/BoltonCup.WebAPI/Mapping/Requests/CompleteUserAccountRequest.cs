5ce69e8d37c2bc6748c34041657842afc2de063f
nic <bolton21@uwindsor.ca>
2026-04-02 11:08:44 -0400

    move CompleteUserAccountRequestValidator into model file

--- src/BoltonCup.WebAPI/Mapping/Requests/CompleteUserAccountRequest.cs
+++ src/BoltonCup.WebAPI/Mapping/Requests/CompleteUserAccountRequest.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using BoltonCup.Core.Values;
+using FluentValidation;
 
 namespace BoltonCup.WebAPI.Mapping;
 
@@ -35,4 +37,29 @@ public record CompleteUserAccountRequest
     [MaxLength(100)]
     [Display(Name = "Preferred beer")]
     public string? PreferredBeer { get; set; }
+}
+
+public class CompleteUserAccountRequestValidator : AbstractValidator<CompleteUserAccountRequest>
+{
+    public CompleteUserAccountRequestValidator()
+    {
+        RuleFor(x => x.Birthday)
+            .NotEmpty().WithMessage("Birthday is required.")
+            .Must(BeAValidAge).WithMessage("You must be 16+ years old to play.");
+
+        RuleFor(x => x.HighestLevel)
+            .Must(level => SkillLevel.All.Contains(level))
+            .When(x => !string.IsNullOrWhiteSpace(x.HighestLevel))
+            .WithMessage($"Highest level must be one of: {string.Join(", ", SkillLevel.All)}");
+    }
+
+    private bool BeAValidAge(DateTime birthday)
+    {
+        var today = DateTime.Today;
+        var yearsOld = today.Year - birthday.Year;
+        // Go back to the year in which the person was born in case of a leap year
+        if (birthday.Date > today.AddYears(-yearsOld)) 
+            yearsOld--;
+        return yearsOld is >= 16 and <= 80;
+    }
 }
