fb2a03956460b340834bbc1d4c904e28ddde8f86
nic <bolton21@uwindsor.ca>
2026-04-16 10:43:01 -0400

    Core: add game highlights

--- src/BoltonCup.Core/Entities/Player.cs
+++ src/BoltonCup.Core/Entities/Player.cs
@@ -25,6 +25,7 @@ public class Player : EntityBase
     public ICollection<Penalty> Penalties { get; set; } = [];
     public ICollection<SkaterGameLog> SkaterGameLogs { get; set; } = [];
     public ICollection<GoalieGameLog> GoalieGameLogs { get; set; } = [];
+    public ICollection<GameHighlight> GameHighlights { get; set; } = [];
     public ICollection<GameStar> Stars { get; set; } = [];
 
     public override string ToString()
