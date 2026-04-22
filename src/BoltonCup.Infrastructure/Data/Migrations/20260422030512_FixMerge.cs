c9f15d35e5a4d7675cf795e89ee370b3996763a8
nic <bolton21@uwindsor.ca>
2026-04-21 23:07:41 -0400

    fix db ef history

---
+++ src/BoltonCup.Infrastructure/Data/Migrations/20260422030512_FixMerge.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BoltonCup.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixMerge : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "captaincy",
+                schema: "core",
+                table: "players",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldDefaultValue: "None");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "game_type",
+                schema: "core",
+                table: "games",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "captaincy",
+                schema: "core",
+                table: "players",
+                type: "text",
+                nullable: false,
+                defaultValue: "None",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldDefaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "game_type",
+                schema: "core",
+                table: "games",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
