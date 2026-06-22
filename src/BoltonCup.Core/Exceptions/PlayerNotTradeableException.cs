4d459540c1b5a8b3d32504db0555f13b68cccef1
nicbolton <nicbolton@geotab.com>
2026-06-22 08:44:37 -0400

    Simplify PlayerNotTradeableException

--- src/BoltonCup.Core/Exceptions/PlayerNotTradeableException.cs
+++ src/BoltonCup.Core/Exceptions/PlayerNotTradeableException.cs
@@ -1,13 +1,4 @@
 namespace BoltonCup.Core.Exceptions;
 
-public sealed class PlayerNotTradeableException(Player player)
-    : BoltonCupException(GetExceptionMessage(player))
-{
-    static string GetExceptionMessage(Player player)
-    {
-        var playerName = player.Account == null
-            ? $"Player {player.Id}"
-            : player.Account.FirstName + " " + player.Account.LastName;
-        return $"{playerName} cannot be traded.";
-    }
-}
+public sealed class PlayerNotTradeableException(Player player) 
+    : BoltonCupException($"{player} cannot be traded.");
