1f7c77b56bfd7850cacd891fcf26d988c0463908
nicbolton <nicbolton@geotab.com>
2026-06-26 16:45:01 -0400

    Add validation message for alphanumeric password character

--- src/BoltonCup.Auth/Models/CreateAccountWithPasswordFormModel.cs
+++ src/BoltonCup.Auth/Models/CreateAccountWithPasswordFormModel.cs
@@ -13,5 +13,6 @@ public class CreateAccountWithPasswordFormModel
     [Required]
     [DataType(DataType.Password)]
     [MinLength(8, ErrorMessage = "Password must have at least 8 characters")]
+    [RegularExpression(@".*[a-zA-Z0-9].*", ErrorMessage = "Password must contain at least one alphanumeric character")]
     public string Password { get; set; } = string.Empty;
 }
