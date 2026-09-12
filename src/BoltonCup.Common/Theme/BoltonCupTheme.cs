05ea2b002efe9f2793f804440946348529cd1f70
nicbolton <nicbolton@geotab.com>
2026-09-12 10:57:29 -0400

    Fix colors from eb0de39

--- src/BoltonCup.Common/Theme/BoltonCupTheme.cs
+++ src/BoltonCup.Common/Theme/BoltonCupTheme.cs
@@ -9,7 +9,6 @@ public class BoltonCupTheme
         PaletteLight = new PaletteLight
         {
             Primary = "#0B2551",
-            PrimaryContrastText = "#424242",
             Secondary = "#EB443A",
             Tertiary = "#DDCBA4",
             AppbarBackground = "#191A21",
